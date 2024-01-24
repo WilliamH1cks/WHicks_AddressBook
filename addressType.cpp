6065bf404f26fd9fe8d1c216b1cffb42f062ebb7
WilliamH1cks <wch47909@email.vccs.edu>
2024-01-24 11:37:07 -0500

    Added default constructor, constructor with parameters, and get and set fucntions for each variable, alongside a print function.

--- addressType.cpp
+++ addressType.cpp
@@ -1,1 +1,89 @@
 #include "addressType.h"
+#include <iostream>
+using namespace std;
+
+void addressType::print()
+{
+	cout << getAddress() << endl;
+	cout << getCity() << ", " << getState() << " " << getZipcode() << endl;
+}
+
+void addressType::setState(string newSt)
+{
+	if (newSt.length() == 2)
+	{
+		state = newSt;
+	}
+	else
+	{
+		cout << "The state abbriviation, \"" << newSt << "\", is invalid. Deafulting to the abbreviation \"XX\"..." << endl;
+		state = "XX";
+	}
+}
+
+void addressType::setZipcode(int zip)
+{
+	if (zip >= 11111 && zip <= 99999 || zip == 10000)
+	{
+		zipcode = zip;
+		if (zip == 10000)
+		{
+			cout << "Okay look, you put in the default zipcode, which is out-of-range but its gonna be used anyway." << endl;
+		}
+	}
+	else
+	{
+		cout << "The zipcode, " << zip << ", is invalid. Defaulting to zipcode 10000..." << endl;
+		zipcode = 10000;
+	}
+}
+
+void addressType::setAddress(string newAddr)
+{
+	address = newAddr;
+}
+
+void addressType::setCity(string newCity)
+{
+	city = newCity;
+}
+
+//default constructor
+addressType::addressType()
+{
+	address = "(An address)";
+	city = "(A city, county, or town)";
+	state = "XX";
+	zipcode = 10000;
+}
+
+//constructor w/ parameters
+addressType::addressType(string newAddr, string newCity, string newSt, int zip)
+{
+	address = newAddr;
+	city = newCity;
+	state = "XX";
+	setState(newSt);
+	zipcode = 10000;
+	setZipcode(zip);
+}
+
+string addressType::getAddress()
+{
+	return address;
+}
+
+string addressType::getCity()
+{
+	return city;
+}
+
+string addressType::getState()
+{
+	return state;
+}
+
+int addressType::getZipcode()
+{
+	return zipcode;
+}
--- addressType.h
+++ addressType.h
@@ -5,9 +5,44 @@ using namespace std;
 class addressType
 {
 	private:
-		string address;
-		string city;
-		string state;
-		int zipcode;
+		string address = "";
+		string city = "";
+		string state = "XX";
+		int zipcode = 10000;
+	public:
+		void print();
+		addressType();
+
+		//gets individual variables from an addressType object
+		string getAddress();
+		string getCity();
+		string getState();
+		int getZipcode();
+
+		//Precond - given state, city, address, and zipcode values
+		//Postcond - new addressType object is created with new and/or valid values
+		addressType(string newAddr, string newCity, string newSt, int zip);
+
+		// setAddress
+		//Precond - Given value is an address
+		//Postcond - Given value is set as the new address
+		void setAddress(string);
+
+		// setCity
+		//Precond - Given value is city, county, and/or town name
+		//Postcond - Sets city value to new given value
+		void setCity(string);
+
+		// setCity
+		//Precond - 2-letter state abbreivation supplied for state value
+		//Postcond - Sets state variable as either new state value given 
+		//			 or, if not a 2-letter abbreviation, the default XX value
+		void setState(string);
+
+		// setZipcode
+		// Precond - integer between 11111 and 99999 given for new zipcode value
+		// Postcond - New value set as zipcode (if outside of range, default value of 10000 is given)
+		void setZipcode(int);
+
 };
 
